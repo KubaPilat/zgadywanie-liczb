69068743506d146cc1b3d014ba475096623b4eb8
Kuba \Pi <kubapilat80@gmail.com>
2020-12-10 11:48:54 +0000

    V 0.1

--- main.py
+++ main.py
@@ -25,3 +25,7 @@ print("Największa wysolowana liczba to: ", Z)
     #else:
      #   print("Nie zgadłeś. Spróbuj jeszcze raz.")
     #print()
+
+"""
+Na ocenę dobrą:-komputer losuje liczbęz zakresu 1-50-użytkownik próbuj odgadnąć tą liczbę-jeżeli odgaduje, to komunikat BRAWO-w przeciwnym razie komunikat NIESTETYNa ocenę bardzo dobrą:-komputer losuje liczbęz zakresu 1-50-użytkownik próbuj odgadnąć tą liczbę-jeżeli odgaduje, to komunikat BRAWO-w przeciwnym razie komunikat NIESTETYoraz PRÓBUJ DO SKUTKU (program wraca do początku)Na oceną celującą:-komputer losuje pięć liczb(zakres 1-200)-wyznacza największą i najmniejsząz nich-komputer wyświetla komunikat: NAJMNIEJSZA Z WYLOSOWANYCH LICZB TO...NAJWIĘKSZA Z WYLOSOWANYCH LICZB TO...Do 18 grudnia
+"""
